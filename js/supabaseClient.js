87b72a2a1d312c3ffc05e46f9c6395bb70cebe7d
icaro-cell <icaro.o.freitas2005@gmail.com>
2025-07-01 20:18:13 -0300

    nova pagina movimentaçao

--- js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://rbkjiasrkbifhqsccrwv.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJia2ppYXNya2JpZmhxc2Njcnd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTIxMjEsImV4cCI6MjA2NTc2ODEyMX0.zABQUHL22qmWiOpLuIJvHgpWM0ZRtlrNFLcT-oEFbVw';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
